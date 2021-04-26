a49578ae08abfb1d73d0129b40c44aeefda5a7c7
earllee333 <jerrykiet2012@gmail.com>
2021-04-26 21:31:19 +0800

    v1.05

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ const Home = () => {
   return ( 
     <>
       <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <title>Home|page</title>
       </Head>
     
